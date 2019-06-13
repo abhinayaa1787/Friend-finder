7211c229323bac5cdcdbccee6ba76a06e9685928
Abhinayaa <abhinayaa1787@gmail.com>
2019-06-13 16:26:48 -0400

    working app- end result to be displayed in modal

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,18 +19,90 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+res.json(friends);
+
+});
+
+app.post("/api/friends", function(req, res) {
+  friends.push(req.body);
+  var bestMatch=findFriend(req.body);
+  res.send(JSON.stringify(bestMatch));
+});
+  
 
   
-  app.post("/api/friends", function(req, res) {
-    //   console.log(req)
-      friends.push(req.body);
-    //   res.send('success')
-  });
+      function findFriend(person){
+       
+       for(i=0;i<friends.length-1;i++){
+           var diff=0;
+       
+           var friend=friends[i];
+           for(j=0;j<10;j++){
+               
+               var scoreDiff=   Math.abs(person.scores[j]-friend.scores[j]);
+               console.log(scoreDiff);
+       
+           diff=diff+scoreDiff;
+       
+       }
+           console.log(diff+"difference for " +i)    ;
+       
+       // console.log(diff)   ;
+       friend.diff=diff;
+       // friends.push(friend.diff);
+       }
+       friends.sort(function(a, b) {
+           return a.diff - b.diff;
+       });
+       console.log(friends);
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+// var html="<div id='myModal' class='modal fade' role='dialog'>"
+// html+='<div class="modal-dialog">'
+// html+='<div class="modal-content">'
+// html+='<div class="modal-header">'
+// html+='<button type="button" class="close" data-dismiss="modal">&times;</button>'
+// html+='<h4 class="modal-title">Best match</h4>'
+// html+='</div>'
+// html+=' <div class="modal-body">'
+// html+='<p>name:friends[0].name</p>'
+// html+='<p>photo:friends[0].photo</p>'
 
-};
+
+// html+=' <div class="modal-body">'
+// html+=' </div>'
+// html+=' <div class="modal-body">'
+// html+='     <div class="modal-footer">'
+// html+=' <div class="modal-body">'
+// html+='     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'
+// html+=' <div class="modal-body">'
+// html+='</div>'
+// html+=' <div class="modal-body">'
+// html+='   </div>'
+
+// html+=' <div class="modal-body">'
+// html+='</div>'
+// html+=' <div class="modal-body">'
+// html+='</div>'
+
+       var bestMatch={
+         name:friends[0].name,
+         photo:friends[0].photo 
+       }
+       return bestMatch;
+    //    alert("best match"+"\n"+friends[0].name+"\n"+friends[0].photo);
+       
+           }
+          }
+           
+       
+           
+       
+       
+           
+       
+       
+       
+
+
+      
+
